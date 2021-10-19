2efeba0c2252fc0fac059a8b41c9cdfb8ff314e8
Syed Rifat Raiyan <46789391+Starscream-11813@users.noreply.github.com>
2021-10-19 22:16:31 +0600

    Update config.py

--- config.py
+++ config.py
@@ -17,17 +17,14 @@ class Config(object):
 
     #CLIENT_ID = os.environ.get('CLIENT_ID') or '19e83f4b909394d4387535acce0cb16a403985a01f7d.api.hackerearth.com'
     #CLIENT_ID = os.environ.get('CLIENT_ID') or '64533056ec05b9c18aaadd2dc400dccc8c490818d129.api.hackerearth.com'
-    CLIENT_ID = os.environ.get(
-        'CLIENT_ID') or '1d167055e97a90457fb88ef36fc814b8'
+    CLIENT_ID = os.environ.get('CLIENT_ID') or '1d167055e97a90457fb88ef36fc814b8'
     #CLIENT_ID = os.environ.get('CLIENT_ID') or '0214c33a'
     #ACCESS_TOKEN = '31a1e40ec6fab583cb8d2ac529ff44f8'
     #CLIENT_SECRET_KEY = os.environ.get('CLIENT_SECRET_KEY') or '4acde50b23e29070bad8ed489b4dcffc488b6169'
     #CLIENT_SECRET_KEY = os.environ.get('CLIENT_SECRET_KEY') or '3865ac3bf22ade323a143ebc7184cc9ed9000e41'
-    CLIENT_SECRET_KEY = os.environ.get(
-        'CLIENT_SECRET_KEY') or '36547f75e2c06b3f3e62c3d6cc59230253930d0a162ad848fe7c0c9e0c8eada4'
+    CLIENT_SECRET_KEY = os.environ.get('CLIENT_SECRET_KEY') or '36547f75e2c06b3f3e62c3d6cc59230253930d0a162ad848fe7c0c9e0c8eada4'
     #COMPILE_URL = os.environ.get('COMPILE_URL') or 'https://api.hackerearth.com/v3/code/compile/'
-    COMPILE_URL = os.environ.get(
-        'COMPILE_URL') or 'https://api.jdoodle.com/v1/execute'
+    COMPILE_URL = os.environ.get('COMPILE_URL') or 'https://api.jdoodle.com/v1/execute'
     #RUN_URL = os.environ.get('RUN_URL') or 'https://api.hackerearth.com/v3/code/run/'
     #RUN_URL = os.environ.get('RUN_URL') or 'https://api.hackerearth.com/v4/partner/code-evaluation/submissions/'
     #RUN_URL = os.environ.get('RUN_URL') or 'https://0214c33a.compilers.sphere-engine.com/api/v4'
